8abed1f2c1cabd1921b34b7e689fc78e74425837
Seemanapalli <167073198+Seemanapalli@users.noreply.github.com>
2024-04-15 18:07:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,19 +11,12 @@ st.write(
     """
 )
 
-# option = st.selectbox(
-#     'What is your favourite fruit?',
-#     ('Apple', 'Mango', 'Banana','Strawberry','PineApple','Avacado','Sapota'))
-
-# st.write('You selected:', option)
-
 name_on_order = st.text_input('Name on Smoothie')
 st.write('The name on your Smoothie will be', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, max_selections = 5)
 
@@ -31,7 +24,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ''
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+            st.subheader(fruit_chosen + 'Nutrition Information')
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
             fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
