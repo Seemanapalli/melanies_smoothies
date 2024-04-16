7c50547a6fb080aa2be321069319f02d840e8786
Seemanapalli <167073198+Seemanapalli@users.noreply.github.com>
2024-04-16 15:05:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ ingredients_list = st.multiselect('Choose upto 5 ingredients:', my_dataframe, ma
 if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-            ingredients_string += fruit_chosen + ''
+            ingredients_string += fruit_chosen + ' '
 
             search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_chosen)
